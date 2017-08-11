faf0724a2c42890710bb792197327219200c80c9
alanJae <dyjae@vip.qq.com>
2017-08-11 11:18:28 +0800

    翻译跳转
    1.添加翻译的页面跳转

--- src/main/webapp/js/recite.js
+++ src/main/webapp/js/recite.js
@@ -1,6 +1,11 @@
+/*将  所有单词 localWord
+	待背诵单词名 wordArray
+	单词所有翻译 wordTrans
+	当前背诵单抽 loadWord
+从本地内存中取出来*/
 var localWord = JSON.parse(localStorage.getItem("word"));
 var wordArray = JSON.parse(localStorage.getItem("wordArray"));
-//var wordTimes = JSON.parse(localStorage.getItem("wordTimes"));
+var wordTimes = JSON.parse(localStorage.getItem("wordTimes"));
 var wordTrans = JSON.parse(localStorage.getItem("wordTrans"));
 var loadWord = localStorage.getItem("loadWord");
 
@@ -12,12 +17,12 @@ $(document).ready(function(){
 			wordTimes = {}
 			wordTrans = []
 			
-			console.info(result.results)
+			//console.info(result.results)
 			var results = result.results
 			localWord = results
 			for(key in results){
 				wordArray.push(key)
-				//wordTimes[key] = results[key].times
+				wordTimes[key] = results[key].times
 				var resultTrans = results[key].trans
 				for(var loopI in resultTrans){
 					var resultTran = resultTrans[loopI]
@@ -25,16 +30,23 @@ $(document).ready(function(){
 				}
 				
 			}
+			
+			//本地缓存数据
+			//原始单词数据
 			localStorage.setItem("word",JSON.stringify(localWord))
+			//只有单词名称数组
 			localStorage.setItem("wordArray",JSON.stringify(wordArray))
-			//localStorage.setItem("wordTimes",JSON.stringify(wordTimes))
+			localStorage.setItem("wordTimes",JSON.stringify(wordTimes))
+			//当前要背单词
 			localStorage.setItem("loadWord",wordArray[0])
+			//所有翻译数组
 			localStorage.setItem("wordTrans",JSON.stringify(wordTrans))
 			loadWord = wordArray[0]
 			loadPage();
 		}
 	}
 	
+	//缓存中没有单词数据时，向服务器请求数据
 	if(localWord == null || localWord == "" || localWord == "undefine"){
 		$.ajax({
 			type:"post",
@@ -54,7 +66,7 @@ function loadPage(){
 	var now_word = localWord[loadWord]
 	var now_trans = now_word.trans
 	var now_phonetics = now_word.phonetics
-	var now_times = now_word.times
+	var now_times = wordTimes[loadWord].times
 	var trans_array = []
 	var true_tran = now_word.trans[0].part+now_word.trans[0].means
 	
@@ -80,16 +92,136 @@ function loadPage(){
 	trans_array.sort(randomSort);
 	
 	var li_html = ""
+	$(".choose_group").html("")
 	for(var li_tran in trans_array){
-		li_html += '<li class="list-group-item tran_li">'+trans_array[li_tran]+'</li>'
+		li_html = '<li class="list-group-item tran_li" >'+trans_array[li_tran]+'</li>'
+		$(".choose_group").append(li_html);
+		
 	}
-	$(".choose_group").append(li_html);
-	
 	$(".tran_li").bind("click",function(){
-		console.info($(".tran_li").val())
+		var trueText = this.innerText
+		console.info(this.innerText)
+		
+		//总次数+1
+		
+		if(true_tran === trueText){
+			//回答正确的时候
+			//正确次数+1
+			//loadWord 变到下一个
+			var i = wordArray.indexOf(loadWord)
+			loadWord = wordArray[++i]
+			loadPage()
+			
+			//判断是否已经正确10次
+			//正确十次
+			//wordArray  localWord wordTimes 移除该单词
+		}else{
+			$(".recite_page").hide()
+			$(".detail_page").show()
+			//错误次数+1
+			//显示单词详情
+		}
+		
 	})
 	
+	loadDetailPage();
 };
+
+//详情
+function loadDetailPage(){
+	$(".detail_word_txt").html(loadWord)
+	
+	console.info(localWord)
+	
+	var results = localWord[loadWord]
+	
+	var trans = results.trans
+	var word_name = loadWord
+	var phonetics = results.phonetics
+	var ph_other = phonetics.ph_other
+	var ph_en = phonetics.ph_en
+	var ph_en_mp3 = phonetics.ph_en_mp3
+	var ph_other_mp3 = phonetics.ph_other_mp3
+	var ph_am = phonetics.ph_am
+	var ph_am_mp3 = phonetics.ph_am_mp3
+	
+	$(".en-mp3").attr("src","")
+	$(".am-mp3").attr("src","")
+	$(".other-mp3").attr("src","")
+	$(".en-ph").html("无")
+	$(".am-mp3").html("无")
+	//$(".other").html("无")
+
+	var en_ph_html = '英音'
+	if(ph_en != null && ph_en!= ''){
+		en_ph_html += '['+ph_en+']'
+	}
+	if(ph_en_mp3 != null && ph_en_mp3 !== ''){
+		en_ph_html += '<div class="glyphicon glyphicon-volume-up"></div>'
+		$(".en-mp3").attr("src",ph_en_mp3)
+	}
+	
+	var am_ph_html = '美音'
+	if(ph_am != null && ph_am!= ''){
+		am_ph_html += '['+ph_am+']'
+	}
+	if(ph_am_mp3 != null && ph_am_mp3 !== ''){
+		am_ph_html += '<div class="glyphicon glyphicon-volume-up"></div>'
+		$(".am-mp3").attr("src",ph_am_mp3)
+	}
+	
+	if(ph_en_mp3 == "undefined" && ph_am_mp3 == "undefined" && ph_other_mp3 != "undefined"){
+		en_ph_html += '<div class="glyphicon glyphicon-volume-up"></div>'
+		$(".en-mp3").attr("src",ph_other_mp3)
+	}
+	
+	if(en_ph_html === "英音"){
+		en_ph_html = "无"
+	}
+	if(am_ph_html === "美音"){
+		am_ph_html = "无"
+	}
+	$(".en-ph").html(en_ph_html)
+	$(".am-ph").html(am_ph_html)
+	$(".en-ph").show()
+	$(".am-ph").show()
+	$(".content").show()
+	
+	var appendHtml = ""
+	for(var i in trans){
+		var tran = trans[i]
+		appendHtml += '<li class="list-group-item">'+tran.part+tran.means+'</li>'
+	}
+	$(".detail_trans").html(appendHtml)
+	$(".content-trans").show()
+}
+
+$(".en-ph").bind("click",function(){
+	var mp3 = $(".en-mp3").attr("src")
+	audio = document.getElementById('enmp3')
+	if(mp3!= null && mp3!= ''){
+		audio.play()
+	}
+})
+
+$(".am-ph").bind("click",function(){
+	var mp3 = $(".am-mp3").attr("src")
+	audio = document.getElementById('ammp3')
+	if(mp3!= null && mp3!= ''){
+		audio.play()
+	}
+})
+
+
+$(".detail_word").bind("click",function(){
+	$(".recite_page").show()
+	$(".detail_page").hide()
+})
+
+$(".content-trans").bind("click",function(){
+	$(".recite_page").show()
+	$(".detail_page").hide()
+})
 
 
 function RandomNum(Min, Max) {
@@ -102,3 +234,4 @@ function RandomNum(Min, Max) {
 function randomSort(a, b) {
 	return Math.random() > 0.5 ? -1 : 1;
 }
+
